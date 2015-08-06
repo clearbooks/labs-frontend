39cd25e97a7bf61ca15849d3c27446d4a3876b85
Fiona Przybylski <fiona@clearbooks.co.uk>
2015-08-06 16:54:28 +0100

    creating select through ng-options

--- app/scripts/controllers/dashboard.ts
+++ app/scripts/controllers/dashboard.ts
@@ -15,5 +15,60 @@ module labsFrontendApp {
   }
 }
 
+var labsFrontendApp = angular.module('labsFrontendApp', []);
+
+labsFrontendApp.controller('DashboardCtrl', function ($scope) {
+
+  $scope.sections = [
+    {
+      id: '1',
+      title: "First feature"
+    },
+    {
+      id: '2',
+      title: "Second feature"
+    }
+  ];
+
+  $scope.section = 'test';
+
+});
+
+labsFrontendApp.controller('ItemController', ['$scope', function ($scope) {
+  $scope.features = [
+    {name: 'feature one'},
+    {name: 'feature two', notAnOption: true},
+    {name: 'feature three'},
+    {name: 'feature four'},
+    {name: 'feature five', notAnOption: false},
+    {name: 'feature six'}
+  ];
+  $scope.myFeature = $scope.features[2];
+}]);
+
+
+
+/*original code below*/
+
+/*angular.module('labsFrontendApp')
+    .controller('DashboardCtrl', labsFrontendApp.DashboardCtrl);*/
+
+/*
 angular.module('labsFrontendApp')
-  .controller('DashboardCtrl', labsFrontendApp.DashboardCtrl);
+    .controller('MainSelectCtrl', ['$scope', function($scope) {
+      $scope.features = [
+        {name:'feature one'},
+        {name:'feature two',  notAnOption: true},
+        {name:'feature three'},
+        {name:'feature four'},
+        {name:'feature five', notAnOption: false},
+        {name:'feature six'}
+      ];
+      $scope.myFeature = $scope.features[2];
+    }]);
+*/
+
+
+
+
+
