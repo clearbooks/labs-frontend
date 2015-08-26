80388641e2dabb7d410e42d9f43d5cd2d360e082
Tom <tom@clearbooks.co.uk>
2015-08-26 14:36:06 +0100

    use a different tsdrc format

--- authGithubTsd.sh
+++ authGithubTsd.sh
@@ -1,7 +1,7 @@
 if [ "$TRAVIS_SECURE_ENV_VARS" = "true" ];
 then
-echo '{ "config": {"github-oauth":{"github.com": ' > ~/.tsdrc
+echo '{"token": ' > ~/.tsdrc
 echo "\"$TSD_GITHUB_TOKEN\"" >> ~/.tsdrc
-echo '}}}' >> ~/.tsdrc
+echo '}' >> ~/.tsdrc
 fi
 
