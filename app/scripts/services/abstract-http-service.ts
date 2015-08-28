f38cc87762c20f61ef1e09bb69fb606ee26079c0
Tom <tom@clearbooks.co.uk>
2015-08-28 11:10:40 +0100

    Populate toggles from the API

---
+++ app/scripts/services/abstract-http-service.ts
@@ -0,0 +1,34 @@
+module labsFrontendApp
+{
+    export class HttpService<T>
+    {
+        /**
+         * The URL to use
+         * @type {string}
+         */
+        protected url: string;
+
+        /**
+         * @param $http
+         * @param $q
+         * @param apiUrl
+         * @ngInject
+         */
+        constructor( private $http: ng.IHttpService, private $q: ng.IQService, private apiUrl: string )
+        {
+        }
+
+        /**
+         *
+         * @param params
+         * @returns {IPromise<TResult>}
+         */
+        protected get( params: Object ): ng.IPromise<T>
+        {
+            return this.$http.get( this.apiUrl + this.url, { params: params } ).then( ( stuff: any ) => {
+                return stuff.data;
+            } );
+        }
+    }
+
+}
