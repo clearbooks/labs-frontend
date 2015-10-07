8e387bfbac393efebed403ff9e27eefdc45a33ad
Fiona Przybylski <fiona@clearbooks.co.uk>
2015-10-07 16:07:20 +0100

    tweaks to select dropdown and nav in mobile mode

--- app/scripts/config/config.js
+++ app/scripts/config/config.js
@@ -1,6 +1,15 @@
+/*
 var config = {
     jwtServer: '{{ jwt_server_url }}',
     accountApiHost: '{{ account_api_host }}',
     accountApiEndpoint: '{{ account_api_endpoint }}'
 };
+*/
+
+var config = {
+    jwtServer: 'https://secure.clearbooks.local/accounting/auth/auth?app=labs&group=17',
+    accountApiHost: 'https://api.clearbooks.local',
+    accountApiEndpoint: '/groups/list'
+};
+
 
