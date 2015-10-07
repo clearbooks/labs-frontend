dac77e21303f132fe22e45e407c9a73ef423239a
Fiona Przybylski <fiona@clearbooks.co.uk>
2015-10-07 16:25:49 +0100

    replace original config configuration

--- app/scripts/config/config.js
+++ app/scripts/config/config.js
@@ -1,15 +1,8 @@
-/*
 var config = {
     jwtServer: '{{ jwt_server_url }}',
     accountApiHost: '{{ account_api_host }}',
     accountApiEndpoint: '{{ account_api_endpoint }}'
 };
-*/
 
-var config = {
-    jwtServer: 'https://secure.clearbooks.local/accounting/auth/auth?app=labs&group=17',
-    accountApiHost: 'https://api.clearbooks.local',
-    accountApiEndpoint: '/groups/list'
-};
 
 
