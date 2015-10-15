6b117a3dce1f35393a632f20bebd7cc19211f342
Fiona Przybylski <fiona@clearbooks.co.uk>
2015-10-15 16:25:08 +0100

    replaced config files

--- app/scripts/config/config.js
+++ app/scripts/config/config.js
@@ -1,14 +1,9 @@
-/*var config = {
+var config = {
     jwtServer: '{{ jwt_server_url }}',
     accountApiHost: '{{ account_api_host }}',
     accountApiEndpoint: '{{ account_api_endpoint }}'
-};*/
-
-var config = {
-    jwtServer: 'https://secure.clearbooks.local/accounting/auth/auth?app=labs&group=17',
-    accountApiHost: 'https://api.clearbooks.local',
-    accountApiEndpoint: '/groups/list'
 };
+
 
 
 
