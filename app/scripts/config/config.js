d4ad8d2e6567e843fe6e805e7c4c907753d8562d
Patrick Kerrigan <patrick@clearbooks.co.uk>
2023-06-19 12:29:33 +0100

    Make API URL configurable

--- app/scripts/config/config.js
+++ app/scripts/config/config.js
@@ -1,5 +1,7 @@
 var config = {
     jwtServer: '{{ jwt_server_url }}',
+    apiHost: '{{ api_host }}',
+    apiBasePath: '{{ api_base_path }}',
     accountApiHost: '{{ account_api_host }}',
     accountApiEndpoint: '{{ account_api_endpoint }}'
 };
